cc898b798c5bc72c35615f9896829f7b63d99e71
Andrew Yourtchenko <ayourtch@gmail.com>
2014-09-28 01:32:50 +0200

    populate the db from mdns and show in webpage

--- example-dns/data/index.lua
+++ example-dns/data/index.lua
@@ -4,18 +4,10 @@ local sqlite3 = require("lsqlite3")
 
 function request(idx)
   local reply = "Hello from lua, index: " .. idx .. "!"
-  local db = sqlite3.open_memory()
+  local db = sqlite3.open('test.db')
 
-  db:exec[[
-    CREATE TABLE test (id INTEGER PRIMARY KEY, content);
-
-    INSERT INTO test VALUES (NULL, 'Hello World');
-    INSERT INTO test VALUES (NULL, 'Hello Lua');
-    INSERT INTO test VALUES (NULL, 'Hello Sqlite3')
-  ]]
-
-  for row in db:nrows("SELECT * FROM test") do
-    reply = reply .. "\n" .. row.id .. "=" .. row.content
+  for row in db:nrows("SELECT * FROM foo;") do
+    reply = reply .. "\n" .. row.name
   end
 
   local headers = "HTTP/1.0 200 OK\r\nConnection: keep-alive\r\nContent-length: " .. #reply .. "\r\nContent-type: text/plain\r\n\r\n"
--- example-dns/example-dns.lua
+++ example-dns/example-dns.lua
@@ -1,6 +1,11 @@
 local dns = require("dns")
 local inspect = require("inspect")
 local sqlite3 = require("lsqlite3")
+local dnsdb = sqlite3.open('test.db')  
+
+dnsdb:exec("create table foo (name string(255));")
+
+--        myDB:close()  
 
 function udp53(srcaddr, srcport, data)
   print("Received udp53 from ", srcaddr, srcport)
@@ -10,11 +15,22 @@ function udp53(srcaddr, srcport, data)
 end
 
 function udp5353(srcaddr, srcport, data)
+  local stmt = dnsdb:prepare("insert into foo values (?1);")
+
   print("Received udp5353 from ", srcaddr, srcport)
   dns._resolver.time = 0
   local resp = dns._resolver:decode(data, true)
-  -- print(inspect(resp))
+  dnsdb:exec("begin transaction;")
+  for i,v in ipairs(resp.answer) do
+    print(i, inspect(v.name))
+    stmt:bind(1, v.name)
+    print(inspect(stmt:step()))
+    stmt:reset()
+  end
+  dnsdb:exec("commit;")
+  -- dnsdb:close()
 end
+
 
 
 
